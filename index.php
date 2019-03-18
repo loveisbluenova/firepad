aa0094793c7c07cbe2dae6856ad02195ffe2bf70
Sasha <sasha.saphon@gmail.com>
2019-03-18 06:28:22 -0400

    version 2

--- index.php
+++ index.php
@@ -23,6 +23,16 @@
       height: 100%;
     }
   </style>
+  <script type="text/javascript">
+    function dataURLtoBlob(dataurl) {
+      var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+          bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+      while(n--){
+          u8arr[n] = bstr.charCodeAt(n);
+      }
+      return new Blob([u8arr], {type:mime});
+    }
+  </script>
 </head>
 
 <body onload="init()">
@@ -44,37 +54,87 @@
       var firepadRef = getExampleRef();
       //// Create CodeMirror (with lineWrapping on).
       var codeMirror = CodeMirror(document.getElementById('firepad-container'), { lineWrapping: true });
-      //// Create Firepad (with rich text toolbar and shortcuts enabled).
+      //// Create Firepad (with rich text  firepad.insertEntity and shortcuts enabled).
       var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
           { richTextToolbar: true, richTextShortcuts: true });
       //// Initialize contents.
       firepad.on('ready', function() {
+        var toolbar_warp = document.getElementsByClassName("firepad-toolbar-wrapper");
+        var upload_btn = '<div class="firepad-btn-group"><a class="firepad-btn" id="upload-image"><span class="firepad-tb-upload-image">Upload</span></a></div>';
+        var div = document.createElement('div');
+        div.innerHTML = upload_btn.trim();
+        var upload_btn_dom = div.firstChild;
+        toolbar_warp[0].appendChild(upload_btn_dom);
+        document.getElementById("upload-image").addEventListener("click", uploadImages);
+        function uploadImages(e) {
+          var firepad_html = firepad.getHtml();
+          var firedom = document.createElement('div');
+          firedom.innerHTML = firepad_html.trim();
+          var firepad_dom = firedom.firstChild;
+          var imgs = firepad_dom.getElementsByTagName('img');
+          for (var i = 0; i < imgs.length; i++) {
+            var img = imgs[i];
+            var dataurl = img.currentSrc;
+            var blob = dataURLtoBlob(dataurl);
+            
+            var filename = Math.floor((1 + Math.random()) * 0x10000).toString(16)+".png";
 
+            var file = new File([blob], filename);
+            console.log(file);
+            var xhr = new XMLHttpRequest();
+            xhr.onload = function() {
+                if (xhr.status == 200) {
+                    console.log("uploaded.");
+                } else {
+                    console.log("oops, something wrong.")
+                }
+            };
+            xhr.onerror = function() {
+                console.log("cannot connect to server.");
+            };
+            xhr.open("POST", "upload.php?filename="+filename, true);
+            xhr.setRequestHeader("Content-Type", file.type);
+            xhr.send(file);
+
+          }
+        }
         document.getElementById("firepad-container").addEventListener("paste", pasteHandler);
         function pasteHandler(e) {
 
-          var filename = Math.floor((1 + Math.random()) * 0x10000).toString(16)+".png";
+          
           var items = e.clipboardData.items;
           for (var i = 0 ; i < items.length ; i++) {
               var item = items[i];
               if (item.type.indexOf("image") >=0) {
-                  var xhr = new XMLHttpRequest();
-                  xhr.onload = function() {
-                      if (xhr.status == 200) {
-                          console.log("uploaded.");
-                          firepad.insertEntity('img', { 
-                            'src' : '/uploads/'+filename,
-                          });
-                      } else {
-                          console.log("oops, something wrong.")
-                      }
-                  };
-                  xhr.onerror = function() {
-                      console.log("cannot connect to server.");
-                  };
-                  xhr.open("POST", "upload.php?filename="+filename, true);
-                  xhr.setRequestHeader("Content-Type", item.getAsFile().type);
-                  xhr.send(item.getAsFile());
+                  file = item.getAsFile()
+                  var reader = new FileReader();
+                  reader.addEventListener("load", function () {
+                    firepad.insertEntity('img', { 
+                      'src' : reader.result,
+                    });
+                  }, false);
+
+                  if (file) {
+                    reader.readAsDataURL(file);
+                  }
+                  // console.log(reader.readAsDataURL(item.getAsFile()));
+                  // var xhr = new XMLHttpRequest();
+                  // xhr.onload = function() {
+                  //     if (xhr.status == 200) {
+                  //         console.log("uploaded.");
+                  //         firepad.insertEntity('img', { 
+                  //           'src' : '/uploads/'+filename,
+                  //         });
+                  //     } else {
+                  //         console.log("oops, something wrong.")
+                  //     }
+                  // };
+                  // xhr.onerror = function() {
+                  //     console.log("cannot connect to server.");
+                  // };
+                  // xhr.open("POST", "upload.php?filename="+filename, true);
+                  // xhr.setRequestHeader("Content-Type", item.getAsFile().type);
+                  // xhr.send(item.getAsFile());
               } else {
                   console.log("Ignoring non-image.");
               }
@@ -167,3 +227,4 @@
 </body>
 </html>
 
+ 
